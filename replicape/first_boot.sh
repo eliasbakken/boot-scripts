7f3c5ed21be3f7ee08d66d503e4a651f0675e0cb
root <root@kamikaze.localdomain>
2015-06-19 12:35:16 +0000

    Updated octoprint config file

--- replicape/first_boot.sh
+++ replicape/first_boot.sh
@@ -10,18 +10,7 @@ chown octo:octo /usr/share/models
 chmod 777 /usr/share/models
 
 # Make config file for Octoprint
-echo "cura:"					 > "/home/octo/.octoprint/config.yaml"
-echo "  config: /etc/cura/printer.ini"		>> "/home/octo/.octoprint/config.yaml"
-echo "  enabled: true" 				>> "/home/octo/.octoprint/config.yaml"
-echo "  path: /usr/share/cura/cura.sh"		>> "/home/octo/.octoprint/config.yaml"
-echo "folder:"					>> "/home/octo/.octoprint/config.yaml"
-echo "  uploads: /usr/share/models"		>> "/home/octo/.octoprint/config.yaml"
-echo "serial:"					>> "/home/octo/.octoprint/config.yaml"
-echo "  additionalPorts:"			>> "/home/octo/.octoprint/config.yaml"
-echo "    - /dev/octoprint_1"			>> "/home/octo/.octoprint/config.yaml"
-echo "  baudrate: 115200"			>> "/home/octo/.octoprint/config.yaml"
-echo "  port: /dev/octoprint_1"			>> "/home/octo/.octoprint/config.yaml"
-
+cp /opt/scripts/replicape/config.yaml /home/octo/.octoprint/
 chown octo:octo "/home/octo/.octoprint/config.yaml"
 
 # Run depmod to fix 
